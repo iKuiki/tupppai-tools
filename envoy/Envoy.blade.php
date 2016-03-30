b471436d729fcb5c39e51c2d4bd556e3000f7794
nunqiang <whenjonny@gmail.com>
2016-03-30 18:34:03 +0800

    版本发布

--- envoy/Envoy.blade.php
+++ envoy/Envoy.blade.php
@@ -1,6 +1,6 @@
 @include('config.php');
 
-@servers([ 'web-dev' => 'jq@tapi.tupppai.com', 'apk-dev' => '127.0.0.1', 'apk-production' => '127.0.0.1', 'web-production1' => 'root@www.tupppai.com', 'web-production2' => 'ubuntu@www.tupppai.com'])
+@servers([ 'web-dev' => 'jq@tapi.tupppai.com', 'apk-dev' => '127.0.0.1', 'apk-production' => '127.0.0.1', 'web-1' => 'ubuntu@www.tupppai.com', 'web-2' => 'ubuntu@www.qiupsdashen.com', 'web-3' => 'ubuntu@www.chupinlm.com'])
 
 @task('web-deploy', ['on' => 'web-dev', 'confirm' => false])
     cd {{$webPath.$tupppaiPath}}
@@ -8,26 +8,24 @@
     git pull origin develop
     php artisan migrate
     php artisan db:seed
-    cp -r public/src/dist/* public/
 @endtask
 
-@task('web-publish', ['on' => 'web-production1', 'confirm' => true])
+@task('web-publish', ['on' => ['web-1', 'web-2', 'web-3'], 'confirm' => true])
     cd {{$webPath.$tupppaiPath}}
     cp -r {{$webPath.$tupppaiPath}} {{$backPath}}/{{$tupppaiPath}}_{{$date}}
     git checkout master
     git pull origin master
     php artisan migrate
-    cp -r public/src/dist/* public/
 @endtask
 
 @task('design-deploy', ['on' => 'web-dev', 'confirm' => true])
     cd {{$webPath.$designPath}}
-    git checkout master
-    git pull origin master
+    git checkout develop
+    git pull origin develop
     php artisan migrate
 @endtask
 
-@task('design-publish', ['on' => 'web-production1', 'confirm' => true])
+@task('design-publish', ['on' => ['web-1', 'web-2', 'web-3'], 'confirm' => true])
     cd {{$webPath.$designPath}}
     cp -r {{$webPath.$designPath}} {{$backPath}}/{{$designPath}}_{{$date}}
     git checkout master
